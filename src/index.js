1b6ee7c67f5ab9108f18f924879c2b61f13e42d7
Jack Baron <jackmbaron@gmail.com>
2019-02-01 12:55:04 +0000

    Add commands

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const log = require('fancylog')
 const dotenv = require('dotenv')
 const exitHook = require('async-exit-hook')
 const { Client } = require('discord.js')
+const { minesweeper } = require('./minesweeper.js')
 
 // Load Environment Variables
 dotenv.load()
-const { TOKEN } = process.env
+const { TOKEN, PREFIX } = process.env
+const prefix = PREFIX || '!'
 
 // Create the Bot Client
 const client = new Client()
@@ -15,6 +17,34 @@ client.login(TOKEN)
 // Boring Handlers
 client.on('ready', () => log.info(`Connected to Discord as ${client.user.tag}`))
 client.on('error', err => log.error(err))
+
+// Message Handler
+client.on('message', async message => {
+  if (message.author.bot) return undefined
+  if (!message.content.startsWith(prefix)) return undefined
+
+  // Check we have perms to send messages
+  const perms = message.channel.permissionsFor(client.user)
+  if (!perms.has('SEND_MESSAGES')) return undefined
+
+  const command = message.content.replace(prefix, '')
+  if (command === 'minesweeper') {
+    // Generate Board
+    const board = minesweeper()
+    return message.channel.send(board)
+  } else if (command === 'minesweeper invite') {
+    // Send invite link
+    const permissions = [
+      'SEND_MESSAGES',
+      'READ_MESSAGE_HISTORY',
+    ]
+
+    const invite = await client.generateInvite(permissions)
+    return message.channel.send(`<${invite}>`)
+  } else {
+    return undefined
+  }
+})
 
 // Handle SIGTERM and SIGINT signals properly
 exitHook(async exit => {
