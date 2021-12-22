c8dba96bd3bdf5692ab02a163e55256983053a79
Jack Baron <jackmbaron@gmail.com>
2021-12-22 03:07:37 +0000

    handle interaction ping requests

--- src/api/interaction.ts
+++ src/api/interaction.ts
@@ -1,5 +1,43 @@
 import nc from 'next-connect'
+import getRawBody from 'raw-body'
+import { verifyKey } from 'discord-interactions'
+import { type VercelRequest, type VercelResponse } from '@vercel/node'
+import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import { InteractionResponseType, InteractionType, type APIMessageInteraction } from 'discord-api-types/payloads/v9'
 
-const handler = nc()
+const extractHeader: (header: string | string[] | undefined) => string | undefined = header => Array.isArray(header) ? header[0] : header
+
+const handler = nc<VercelRequest, VercelResponse>()
+handler.post(async (request, response) => {
+  const signature = extractHeader(request.headers['x-signature-ed25519'])
+  if (!signature) {
+    response.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN)
+    return
+  }
+
+  const timestamp = extractHeader(request.headers['x-signature-timestamp'])
+  if (!timestamp) {
+    response.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN)
+    return
+  }
+
+  const rawBody = await getRawBody(request)
+  const isValid = verifyKey(
+    rawBody,
+    signature,
+    timestamp,
+    process.env.PUBLIC_KEY!
+  )
+
+  if (!isValid) {
+    response.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN)
+    return
+  }
+
+  const message = request.body as APIMessageInteraction
+  if (message.type === InteractionType.Ping) {
+    response.send({ type: InteractionResponseType.Pong })
+  }
+})
 
 export default handler
